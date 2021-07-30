37c7a67a8edeafbdb6d5660d214309181db32fa4
wc <wchamberlain89@gmail.com>
2021-07-30 10:17:50 -0700

    Completed Project

---
+++ src/script.js
@@ -0,0 +1,177 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+
+/**
+ * Loaders
+ */
+
+const gltfLoader = new GLTFLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI()
+
+// const customContainer = document.getElementById('gui-container');
+// customContainer.appendChild(gui.domElement);
+const debugObject = {}
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+
+
+/**
+ * Update All Materials
+ */
+
+const updateAllMaterials = () => {
+    scene.traverse( child => {
+        if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
+            child.material.envMap = environmentMap
+            child.material.envMapIntensity = debugObject.envMapIntensity
+            child.material.needsUpdate = true
+            child.castShadow = true
+            child.receiveShadow = true
+        }
+    })
+}
+/**
+ * Environment Map
+ */
+const environmentMap = cubeTextureLoader.load([
+     'textures/environmentMaps/0/px.jpg',
+     'textures/environmentMaps/0/nx.jpg',
+     'textures/environmentMaps/0/py.jpg',
+     'textures/environmentMaps/0/ny.jpg',
+     'textures/environmentMaps/0/pz.jpg',
+     'textures/environmentMaps/0/nz.jpg',
+    ])
+    
+    scene.background = environmentMap
+    scene.environmentMap = environmentMap
+    debugObject.envMapIntensity = 1
+    gui.add(debugObject, 'envMapIntensity').min(0).max(10).step(.001).onChange(updateAllMaterials)
+    /**
+ * Models
+ */
+
+gltfLoader.load(
+    '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+    (gltf) => {
+        gltf.scene.scale.set(10, 10, 10)
+        gltf.scene.position.set(0, -4, 0)
+        gltf.scene.rotation.y = Math.PI * 0.5
+        scene.add(gltf.scene)
+
+        gui.add(gltf.scene.rotation, 'y').min(- Math.PI).max(Math.PI).step(0.001).name('Y Rotation')
+
+        updateAllMaterials()    
+    },
+)
+
+/** 
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Lights
+ */
+
+const directionalLight = new THREE.DirectionalLight('#FFFFFF', 4)
+directionalLight.position.set(0.25, 3, -2.25)
+directionalLight.castShadow = true
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.mapSize.set(1024, 1024)
+scene.add(directionalLight)
+
+const directionLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+scene.add(directionLightCameraHelper)
+
+gui.add(directionalLight, 'intensity').min(0).max(100).step(0.001).name('Light Intensity')
+gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001).name('Light X')
+gui.add(directionalLight.position, 'y').min(-5).max(5).step(0.001).name('Light Y')
+gui.add(directionalLight.position, 'z').min(-5).max(5).step(0.001).name('Light Z')
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(4, 1, - 4)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.physicallyCorrectLights = true
+renderer.outputEncoding = THREE.sRGBEncoding
+renderer.toneMapping = THREE.ACESFilmicToneMapping
+renderer.toneMappingExposure = 1.85
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+gui.add(renderer, 'toneMapping', {
+    No: THREE.NoToneMapping,
+    Reinhard: THREE.ReinhardToneMapping,
+    Linear: THREE.LinearToneMapping,
+    Cineon: THREE.CineonToneMapping,
+    ACESFilmic: THREE.ACESFilmicToneMapping
+}).onFinishChange(() => {
+    renderer.toneMapping = Number(renderer.toneMapping)
+    updateAllMaterials()
+})
+
+gui.add(renderer, 'toneMappingExposure').min(0).max(10).step(.001)
+/**
+ * Animate
+ */
+const tick = () =>
+{
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
